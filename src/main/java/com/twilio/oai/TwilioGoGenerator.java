a4da0967a6cbdffe0ac5e76b351eda4ad7255479
Sam Harrison <sharrison@twilio.com>
2020-09-28 12:27:18 -0500

    initial TwilioGoGenerator

--- src/main/java/com/twilio/oai/TwilioGoGenerator.java
+++ src/main/java/com/twilio/oai/TwilioGoGenerator.java
@@ -1,198 +1,89 @@
 package com.twilio.oai;
 
-import org.openapitools.codegen.*;
-import io.swagger.models.properties.*;
+import org.openapitools.codegen.CodegenConstants;
+import org.openapitools.codegen.CodegenOperation;
+import org.openapitools.codegen.CodegenParameter;
+import org.openapitools.codegen.SupportingFile;
+import org.openapitools.codegen.languages.GoClientCodegen;
 
-import java.util.*;
-import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class TwilioGoGenerator extends DefaultCodegen implements CodegenConfig {
+public class TwilioGoGenerator extends GoClientCodegen {
 
-  // source folder where to write the files
-  protected String sourceFolder = "src";
-  protected String apiVersion = "1.0.0";
+    public TwilioGoGenerator() {
+        super();
 
-  /**
-   * Configures the type of generator.
-   * 
-   * @return  the CodegenType for this generator
-   * @see     org.openapitools.codegen.CodegenType
-   */
-  public CodegenType getTag() {
-    return CodegenType.OTHER;
-  }
-
-  /**
-   * Configures a friendly name for the generator.  This will be used by the generator
-   * to select the library with the -g flag.
-   * 
-   * @return the friendly name for the generator
-   */
-  public String getName() {
-    return "twilio-go";
-  }
-
-  /**
-   * Provides an opportunity to inspect and modify operation data before the code is generated.
-   */
-  @SuppressWarnings("unchecked")
-  @Override
-  public Map<String, Object> postProcessOperationsWithModels(Map<String, Object> objs, List<Object> allModels) {
-
-    // to try debugging your code generator:
-    // set a break point on the next line.
-    // then debug the JUnit test called LaunchGeneratorInDebugger
-
-    Map<String, Object> results = super.postProcessOperationsWithModels(objs, allModels);
-
-    Map<String, Object> ops = (Map<String, Object>)results.get("operations");
-    ArrayList<CodegenOperation> opList = (ArrayList<CodegenOperation>)ops.get("operation");
-
-    // iterate over the operation and perhaps modify something
-    for(CodegenOperation co : opList){
-      // example:
-      // co.httpMethod = co.httpMethod.toLowerCase();
+        embeddedTemplateDir = templateDir = "twilio-go";
     }
 
-    return results;
-  }
+    @Override
+    public void processOpts() {
+        super.processOpts();
 
-  /**
-   * Returns human-friendly help for the generator.  Provide the consumer with help
-   * tips, parameters here
-   * 
-   * @return A string value for the help message
-   */
-  public String getHelp() {
-    return "Generates a twilio-go client library.";
-  }
+        additionalProperties.put(CodegenConstants.IS_GO_SUBMODULE, true);
 
-  public TwilioGoGenerator() {
-    super();
+        supportingFiles.clear();
+        supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
+        supportingFiles.add(new SupportingFile("configuration.mustache", "", "configuration.go"));
+        supportingFiles.add(new SupportingFile("response.mustache", "", "response.go"));
+        supportingFiles.add(new SupportingFile("go.mod.mustache", "", "go.mod"));
+        supportingFiles.add(new SupportingFile("go.sum", "", "go.sum"));
+    }
 
-    // set the output folder here
-    outputFolder = "generated-code/twilio-go";
+    @SuppressWarnings("unchecked")
+    @Override
+    public Map<String, Object> postProcessOperationsWithModels(Map<String, Object> objs, List<Object> allModels) {
+        Map<String, Object> results = super.postProcessOperationsWithModels(objs, allModels);
 
-    /**
-     * Models.  You can write model files using the modelTemplateFiles map.
-     * if you want to create one template for file, you can do so here.
-     * for multiple files for model, just put another entry in the `modelTemplateFiles` with
-     * a different extension
-     */
-    modelTemplateFiles.put(
-      "model.mustache", // the template to use
-      ".sample");       // the extension for each file to write
+        Map<String, Object> ops = (Map<String, Object>) results.get("operations");
+        ArrayList<CodegenOperation> opList = (ArrayList<CodegenOperation>) ops.get("operation");
 
-    /**
-     * Api classes.  You can write classes for each Api file with the apiTemplateFiles map.
-     * as with models, add multiple entries with different extensions for multiple files per
-     * class
-     */
-    apiTemplateFiles.put(
-      "api.mustache",   // the template to use
-      ".sample");       // the extension for each file to write
+        // iterate over the operation and perhaps modify something
+        for (CodegenOperation co : opList) {
+            System.out.println(co);
+        }
 
-    /**
-     * Template Location.  This is the location which templates will be read from.  The generator
-     * will use the resource stream to attempt to read the templates.
-     */
-    templateDir = "twilio-go";
+        return results;
+    }
 
-    /**
-     * Api Package.  Optional, if needed, this can be used in templates
-     */
-    apiPackage = "org.openapitools.api";
+    @Override
+    public void postProcessParameter(CodegenParameter parameter) {
+        // Make sure required non-path params get into the options block.
+        parameter.required = parameter.isPathParam;
+    }
+
+    @Override
+    public Map<String, String> createMapping(String key, String value) {
+        // Optional dependency not needed.
+        if (value.equals("github.com/antihax/optional")) {
+            return new HashMap<>();
+        }
+
+        return super.createMapping(key, value);
+    }
 
     /**
-     * Model Package.  Optional, if needed, this can be used in templates
+     * Configures a friendly name for the generator.  This will be used by the generator
+     * to select the library with the -g flag.
+     *
+     * @return the friendly name for the generator
      */
-    modelPackage = "org.openapitools.model";
+    @Override
+    public String getName() {
+        return "twilio-go";
+    }
 
     /**
-     * Reserved words.  Override this with reserved words specific to your language
+     * Returns human-friendly help for the generator.  Provide the consumer with help
+     * tips, parameters here
+     *
+     * @return A string value for the help message
      */
-    reservedWords = new HashSet<String> (
-      Arrays.asList(
-        "sample1",  // replace with static values
-        "sample2")
-    );
-
-    /**
-     * Additional Properties.  These values can be passed to the templates and
-     * are available in models, apis, and supporting files
-     */
-    additionalProperties.put("apiVersion", apiVersion);
-
-    /**
-     * Supporting Files.  You can write single files for the generator with the
-     * entire object tree available.  If the input file has a suffix of `.mustache
-     * it will be processed by the template engine.  Otherwise, it will be copied
-     */
-    supportingFiles.add(new SupportingFile("myFile.mustache",   // the input template or file
-      "",                                                       // the destination folder, relative `outputFolder`
-      "myFile.sample")                                          // the output file
-    );
-
-    /**
-     * Language Specific Primitives.  These types will not trigger imports by
-     * the client generator
-     */
-    languageSpecificPrimitives = new HashSet<String>(
-      Arrays.asList(
-        "Type1",      // replace these with your types
-        "Type2")
-    );
-  }
-
-  /**
-   * Escapes a reserved word as defined in the `reservedWords` array. Handle escaping
-   * those terms here.  This logic is only called if a variable matches the reserved words
-   * 
-   * @return the escaped term
-   */
-  @Override
-  public String escapeReservedWord(String name) {
-    return "_" + name;  // add an underscore to the name
-  }
-
-  /**
-   * Location to write model files.  You can use the modelPackage() as defined when the class is
-   * instantiated
-   */
-  public String modelFileFolder() {
-    return outputFolder + "/" + sourceFolder + "/" + modelPackage().replace('.', File.separatorChar);
-  }
-
-  /**
-   * Location to write api files.  You can use the apiPackage() as defined when the class is
-   * instantiated
-   */
-  @Override
-  public String apiFileFolder() {
-    return outputFolder + "/" + sourceFolder + "/" + apiPackage().replace('.', File.separatorChar);
-  }
-
-  /**
-   * override with any special text escaping logic to handle unsafe
-   * characters so as to avoid code injection
-   *
-   * @param input String to be cleaned up
-   * @return string with unsafe characters removed or escaped
-   */
-  @Override
-  public String escapeUnsafeCharacters(String input) {
-    //TODO: check that this logic is safe to escape unsafe characters to avoid code injection
-    return input;
-  }
-
-  /**
-   * Escape single and/or double quote to avoid code injection
-   *
-   * @param input String to be cleaned up
-   * @return string with quotation mark removed or escaped
-   */
-  public String escapeQuotationMark(String input) {
-    //TODO: check that this logic is safe to escape quotation mark to avoid code injection
-    return input.replace("\"", "\\\"");
-  }
+    @Override
+    public String getHelp() {
+        return "Generates a Go client library (beta).";
+    }
 }
